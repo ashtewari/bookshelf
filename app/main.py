b1f21c88508c541600a212420a36a352daa35fd0
Ash Tewari <ash.tewari.name@gmail.com>
2024-03-03 02:36:43 -0500

    generate_valid_collection_name

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 import os
 import sys
 import streamlit as st
-import pandas as pd
+import uuid
+import re
 from dotenv import load_dotenv, find_dotenv
 from src.viewer import ChromaDb
 import tempfile
@@ -14,8 +15,11 @@ def main():
 
     st.title("bookshelf", "📚")
 
-    last_used_path = os.environ['MBED:SelectedDatabasePath']
-    path = st.text_input("Database", placeholder="Full path to database directory", value=last_used_path)
+    preferred_data_path = None
+    if 'Bookshelf:PreferredDataPath' in os.environ:
+        preferred_data_path = os.environ['Bookshelf:PreferredDataPath']
+
+    data_path = st.text_input("Database", placeholder="Full path to database directory", value=preferred_data_path or "data")
 
     uploaded_file = st.file_uploader("Choose a file")
     if uploaded_file is not None:
@@ -26,13 +30,13 @@ def main():
                 f.write(uploaded_file.getvalue())       
         st.write(f"Selected file: {tempFilePath}")
 
-        loader = Loader(path)
-        loader.load(tempFilePath, uploaded_file.file_id)
+        loader = Loader(data_path)
+        loader.load(tempFilePath, generate_valid_collection_name(uploaded_file.name))
     
     st.divider()
 
-    if not(path==""):
-        db = ChromaDb(path)
+    if not(data_path==""):
+        db = ChromaDb(data_path)
 
         col1, col2 = st.columns([1,3])
         with col1:
@@ -58,8 +62,31 @@ def main():
                 result_count = 5  # Set a default value if result_count is empty
             result_df = db.query(query, collection_selected, model_name, int(result_count), dataframe=True)
         
-            st.dataframe(result_df, use_container_width=True)
-            
+            st.dataframe(result_df, use_container_width=True)   
+
+def generate_valid_collection_name(input):
+    #Expected collection name that 
+    #(1) contains 3-63 characters, 
+    #(2) starts and ends with an alphanumeric character, 
+    #(3) otherwise contains only alphanumeric characters, underscores or hyphens (-), 
+    #(4) contains no two consecutive periods
+    
+    # minimum 3 characters
+    result = input
+    if len(input) < 3:
+        result = input + "-" + str(uuid.uuid4())
+    
+    # remove spaces
+    result = result.replace(" ", "_")
+    # remove all non-alphanumeric characters
+    result = ''.join(e for e in result if e.isalnum() or e == "_" or e == "-")
+    # remove consecutive periods
+    result = re.sub(r'\.{2,}', '-', result)
+    # max 63 characters
+    result =  result[0:63].lower()
+    
+    return result 
+         
 if __name__ == "__main__":
 
     working_dir = os.path.dirname(os.path.abspath(__file__))
