adc4a203cbbc0c9abe0576aefa7f8fdec66163dd
Ash Tewari <ash.tewari.name@gmail.com>
2024-05-05 20:52:56 -0400

    use kwargs with invoke

--- app/src/langchain.py
+++ app/src/langchain.py
@@ -8,9 +8,23 @@ class llm_openai:
 
     def execute_prompt(self, api_base, api_key, model_name, timeout, prompt, temperature):
         llm = ChatOpenAI(openai_api_base=f"{api_base}", openai_api_key=api_key, model=model_name, request_timeout=timeout) 
-        messages = [HumanMessage(content=[{"type": "text", "text": str(prompt)},])]      
-        response = llm.invoke(input=messages)
+        messages = [HumanMessage(content=[{"type": "text", "text": str(prompt)},])] 
+        kwargs={
+                "temperature": temperature, 
+                "max_tokens": 1000,
+                "presence_penalty":0.0,
+                "frequency_penalty":0.0,
+                "n":1,
+                "top_p":1.0                                    
+                }        
+        # using invoke method
+        response = llm.invoke(input=[messages,], **kwargs)
         return response.content
+        """
+        # using generate method
+        response = llm.generate(messages=[messages], **kwargs)
+        return response.generations[0][0].text 
+        """  
         
     def execute_instruct_prompt(self, api_key, prompt):
         ## using default instruct model
